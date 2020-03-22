6bd90801e4fdab4bbdcb0923ff8d6391145fed22
JkoolCode <junioroblis@gmail.com>
2020-03-21 21:21:54 -0500

    Update Trivial.cpp

--- Approaches/Trivial.cpp
+++ Approaches/Trivial.cpp
@@ -6,7 +6,8 @@ int main()
 {
 	int arr[] = {1, 7, 5, 9, 2, 12, 3};
 	int n = sizeof(arr)/sizeof(arr[0]);
-	int c = 0, k = 2;
+	signed int c = 0;
+	const signed short k = 2;
 	
 	FOR(i, n)
 		FOR(j, n)
